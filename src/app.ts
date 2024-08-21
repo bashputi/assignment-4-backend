3ce708148630930ca0076536b5aac1d2d108bb9e
Rime <rimeislam672@gmail.com>
2024-08-22 00:01:04 +0600

    basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
-const express = require('express')
+import express, {NextFunction, Request, Response} from 'express';
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Wrold')
 })
 
+export default app;
+
+// route not found error 
+app.use((req, res, next) => {
+  res.json({
+    success:false,
+    message: "Route not found"
+  });
+});
+
+// internal server error 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  res.json({
+    success: false,
+    message: "An internal server error occurred.",
+    err: err.message
+  })
+});
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,23 @@
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  });
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+
+async function main() {
+try {
+
+    await mongoose.connect(config.db_url as string);
+
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+      });
+      
+} catch (error) {
+    console.log(error)
+}
+   
+}
+
+
+
+main()
